bad786e11d4f7e7feae3f3a21c723dbe1e0a9fea
Anna Yakubovich <yakubovichannie@gmail.com>
2024-11-27 20:16:19 +0300

    fonts added

--- crm-frontend/js/components/header.js
+++ crm-frontend/js/components/header.js
@@ -1,6 +1,20 @@
 export const renderHeaderElement = () => {
   const bodyEl = document.querySelector("body");
   const headerEl = document.createElement("header");
+  const containerEl = document.createElement("div");
+  const logoEl = document.createElement("img");
+  const searchBarEl = document.createElement("input");
+
+  headerEl.classList.add("header");
+  containerEl.classList.add("container", "header__container");
+  logoEl.classList.add("header__logo");
+  searchBarEl.classList.add("header__search-bar");
+  searchBarEl.placeholder = "Введите запрос";
+
+  logoEl.src = "../assets/img/logo.svg";
+
+  containerEl.append(logoEl, searchBarEl);
+  headerEl.append(containerEl);
 
   bodyEl.append(headerEl);
 }
